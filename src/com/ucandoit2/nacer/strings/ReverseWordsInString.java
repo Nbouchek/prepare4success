5152c627c2b43caf3331208dac79cf5949f6bd84
Nacer Bouchekhima <nbouche@gmail.com>
2018-11-17 21:55:52 +0000

    ReverseWordsInString further improvement.

--- src/com/ucandoit2/nacer/strings/ReverseWordsInString.java
+++ src/com/ucandoit2/nacer/strings/ReverseWordsInString.java
@@ -1,11 +1,33 @@
 package com.ucandoit2.nacer.strings;
 
-
+/**
+ * Given a String of length S, reverse the whole string without reversing the individual words in it. Words are separated by dots.
+ *
+ * Input:
+ * The first line contains T denoting the number of testcases. T testcases follow. Each case contains a string S containing characters.
+ *
+ * Output:
+ * For each test case, in a new line, output a single line containing the reversed String.
+ *
+ * Constraints:
+ * 1 <= T <= 100
+ * 1 <= |S| <= 2000
+ *
+ * Example:
+ * Input:
+ * 2
+ * i.like.this.program.very.much
+ * pqr.mno
+ *
+ * Output:
+ * much.very.program.this.like.i
+ * mno.pqr
+ */
 public class ReverseWordsInString {
     private String solution(String string) {
         System.out.println(string);
-
-        String[] words = string.split(" ");
+        string = string.trim();
+        String[] words = string.split("[.]");
         for (int i = 0; i < words.length/2; i++) {
             String temp = words[i];
             words[i] = words[words.length - 1 - i];
@@ -13,13 +35,13 @@ public class ReverseWordsInString {
         }
 
         StringBuilder result= new StringBuilder();
-        for (String word : words) result.append(word).append(" ");
+        for (String word : words) result.append(word).append(".");
 
-        return result.toString();
+        return result.toString().trim().substring(0, result.length()-1);
     }
 
     public static void main(String[] args) {
-        String string = "one two three";
+        String string = " one.t ";
         System.out.println(new ReverseWordsInString().solution(string));
     }
 }
