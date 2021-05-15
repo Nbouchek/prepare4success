cfcb25052092e402b71b5133b7e21454566bd17b
Nacer Bouchekhima <nbouchek@gmail.com>
2021-05-15 16:40:58 +0100

    Clean up code

--- src/com/leetcode/nacer/TwoSum.java
+++ src/com/leetcode/nacer/TwoSum.java
@@ -44,7 +44,7 @@ public class TwoSum {
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
         int target = 9;
-        System.out.println(Arrays.toString(new TwoSum().twoSum(nums, target) ));
+        System.out.println(Arrays.toString(new TwoSum().twoSum(nums, target)));
     }
 
 }
--- src/com/ucancrackit2/nacer/arrays/MissingNumberInArray.java
+++ src/com/ucancrackit2/nacer/arrays/MissingNumberInArray.java
@@ -3,19 +3,19 @@ package com.ucancrackit2.nacer.arrays;
 /**
  * Given an array C of size N-1 and given that there are numbers from 1 to N with one element missing, the missing
  * number is to be found.
- *
+ * <p>
  * Input:
  * The first line of input contains an integer T denoting the number of test cases.
  * For each test case first line contains N, size of array. The ssubsequent line contains N-1 array elements.
- *
+ * <p>
  * Output:
  * Print the missing number in array.
- *
+ * <p>
  * Constraints:
  * 1 ≤ T ≤ 200
  * 1 ≤ N ≤ 107
  * 1 ≤ C[i] ≤ 107
- *
+ * <p>
  * Example:
  * Input:
  * 2
@@ -23,15 +23,20 @@ package com.ucancrackit2.nacer.arrays;
  * 1 2 3 5
  * 10
  * 1 2 3 4 5 6 7 8 10
- *
+ * <p>
  * Output:
  * 4
  * 9
- *
+ * <p>
  * Explanation:
  * Testcase 1: Given array : 1 2 3 5. Missing element is 4.
  */
 public class MissingNumberInArray {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 6, 7, 8};
+        System.out.println(" Missing number = " + new MissingNumberInArray().solution(array));
+    }
 
     public int solution(int[] A) {
         int length = A.length;
@@ -39,11 +44,6 @@ public class MissingNumberInArray {
         for (int aA : A) {
             sum += aA;
         }
-        return (int) ((length + 1)*(length + 2)/2 - sum);
-    }
-
-    public static void main(String[] args) {
-        int[] array = { 1, 2, 3, 4, 6, 7, 8 };
-        System.out.println(" Missing number = "  + new MissingNumberInArray().solution(array));
+        return (int) ((length + 1) * (length + 2) / 2 - sum);
     }
 }
